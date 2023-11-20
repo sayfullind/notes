106a99ad8bad27dc2ad2bb3e503477a4b086aa21
macloud <sayfullinoff@gmail.com>
2023-11-20 05:01:10 +0300

    done code review in rest-api: - removed unnecessary classes; - added spring-data-rest; - added spring-hal-explorer

--- rest-api/src/main/java/ru/macloud/rest_api/repositories/NoteRepository.java
+++ rest-api/src/main/java/ru/macloud/rest_api/repositories/NoteRepository.java
@@ -1,9 +1,9 @@
 package ru.macloud.rest_api.repositories;
 
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import ru.macloud.rest_api.entities.Note;
 
 
-@Repository
+@RepositoryRestResource
 public interface NoteRepository extends CrudRepository<Note, Long> {}
